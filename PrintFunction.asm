b78c52eb83b5537a3ec5ae44ee95be111712ff89
HAMDANE Sid-Ali <hamdane.sidali@hotmail.com>
2023-08-15 18:34:14 +0200

    adjusts

--- PrintFunction.asm
+++ PrintFunction.asm
@@ -2,17 +2,17 @@
 ; first version of the code is a siple "hello world" programme 
 
 print_message:
-    pusha
+    pusha ; push all the registers into the stack
     mov ah, 0x0e
-    jmp start_function
+    jmp start_function ; jmp to the section
 start_function:
-    mov al, [bx]
-    cmp al, 0
-    je end_function
-    int 0x10
-    inc bx
-    jmp start_function
+    mov al, [bx] ; mov the content of the adress stored in bx in al
+    cmp al, 0    ; compare the value with O
+    je end_function ; if it's equals 0 we jump to the end
+    int 0x10 ; call the interrupt (so xe print the content of al in the screen)
+    inc bx ; increment bx
+    jmp start_function ; loop
 
 end_function:
-    popa
-    ret
+    popa ; pop all the registers from the stack
+    ret  ; return to the adress from which the call instruction is executed
