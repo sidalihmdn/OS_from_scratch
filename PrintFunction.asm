233cb4a6457bb00e71d27e617d1c0d5b57cdfa8d
HAMDANE Sid-Ali <hamdane.sidali@hotmail.com>
2023-08-15 18:31:45 +0200

    added the printing function

--- PrintFunction.asm
+++ PrintFunction.asm
@@ -1,17 +1,18 @@
 ; this function will be used to write to the screen 
 ; first version of the code is a siple "hello world" programme 
 
-mov ah, 0x0e
+print_message:
+    pusha
+    mov ah, 0x0e
+    jmp start_function
+start_function:
+    mov al, [bx]
+    cmp al, 0
+    je end_function
+    int 0x10
+    inc bx
+    jmp start_function
 
-mov al, 'h'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'o'
-int 0x10
-
-jmp $
+end_function:
+    popa
+    ret
