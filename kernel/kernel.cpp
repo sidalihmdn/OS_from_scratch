184ecbf415a269982b4ecabc09cce16baa2a1ff8
HAMDANE Sid-Ali <hamdane.sidali@gmail.com>
2023-08-23 20:22:35 +0200

    added functions to handle some costume exceptions - working on the pic

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,6 +1,7 @@
 #include "../drivers/screen.h"
 #include "../includes/string.h"
 #include "../int/int.h"
+#include "../int/pic.h"
 #include "../drivers/keyboard.h"
 
 #if DEBUG
@@ -11,8 +12,11 @@ void main(){
     clean_screen();
     print_string((char *)"os > ", 5);
     //print_string(int2String(1234),4);
-    set_idt(); 
-    //init_keyboard();
+    set_idt();
+    int a = 12/0;
+    PIC_remap(32,40);
+    init_keyboard();
+    for(;;){}
     //print_char(0, -1 , -1 , '0'+ len((char *)"les"));
     return;
 
