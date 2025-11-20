a24a67a39a6b790eadaf89ab91662d1233cd1d84
Sid-Ali Hamdane <sidali.hamdane@rakuten.com>
2025-11-20 23:58:55 +0100

    fixed some issues + adjustments

--- includes/string.cpp
+++ includes/string.cpp
@@ -1,21 +1,27 @@
 #include "string.h"
+#include "../drivers/screen.h"
 
 /// @brief this function will return the lenght of a given string
 /// @param c the address of the first character of the string
 /// @return the length of the string
-int len(char* c){
+int len(const char* c){
     int i = 0;
-    while (c[i]=!0){
+    while (c[i]!=0){
         i++;
     }
     return i;
+}
+
+void print(const char string[]){
+    print_string(string, len(string));
 }
 
 /// @brief calculate the number of digits in a given number
 /// @param number 
 /// @return the number of digits (example 1234 -> 4)
 int nb_digits(int number){
-    int digits;
+    int digits = 0;
+    if (number == 0) return 1;
     while(number > 0){
         ++digits;
         number=number/10;
@@ -34,20 +40,36 @@ char int2char(int a){
 /// @param a 
 /// @return 
 char *int2String(int a){
+    static char buffer[16]; // Static buffer to avoid malloc
     int digits_number = nb_digits(a);
-    char *c;
     int i = 0;
-    while(a > 0){
-        i++;
-        c[digits_number-i+1]= int2char(a%10);
-        a = a/10 ;
+    
+    if (a == 0) {
+        buffer[0] = '0';
+        buffer[1] = 0;
+        return buffer;
     }
-    c[digits_number+1] = 0;
-    return c;
+    
+    buffer[digits_number] = 0; // Null terminator
+    
+    while(a > 0){
+        buffer[digits_number - 1 - i] = int2char(a % 10);
+        a = a / 10;
+        i++;
+    }
+    return buffer;
 }
 
 
 char *hex2char(char hex){
     return 0;
+}
+
+int strcmp(char* s1, char* s2) {
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    return *(unsigned char*)s1 - *(unsigned char*)s2;
 }
 
--- includes/string.h
+++ includes/string.h
@@ -1,7 +1,9 @@
  // functions declarations
  //
- int len(char *c);
+ int len(const char *c);
+ void print(const char string[]);
  int nb_digits(int number);
  char int2char(int a);
  char *int2String(int a);
  char *hex2char(char hex); 
+ int strcmp(char* s1, char* s2);
