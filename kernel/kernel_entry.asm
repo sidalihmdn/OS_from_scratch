5b3232c85a73c7b62c847a2f5e972454a2286eaa
Sid-Ali Hamdane <sidali.hamdane@rakuten.com>
2025-11-22 14:31:45 +0100

    mdified the kernel entry

--- kernel/kernel_entry.asm
+++ kernel/kernel_entry.asm
@@ -1,4 +1,12 @@
-[bits 32]
-[extern main]
-call main
-jmp $
+; kernel_entry.asm
+bits 32
+
+global _start     ; <-- make _start visible to the linker
+extern main       ; <-- declare main implemented in C/C++
+
+_start:
+    call main     ; call your kernel main function
+    cli           ; disable interrupts
+halt:
+    hlt           ; halt CPU
+    jmp halt      ; infinite loop
