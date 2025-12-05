21582e2cdde6ec759a36223e80c0a0f0172c652a
Sid-Ali Hamdane <sidali.hamdane@rakuten.com>
2025-12-05 19:59:10 +0100

    working on memory management

---
+++ boot/multiboot.cpp
@@ -0,0 +1,4 @@
+#include "../includes/boot/multiboot.h"
+
+uint32_t multiboot_get_total_memory(multiboot_info_t* mb_info){
+    
--- includes/boot/multiboot.h
+++ includes/boot/multiboot.h
@@ -19,3 +19,19 @@ typedef struct{
     uint64_t len;
     uint32_t type;
 } __attribute__((packed)) mmap_entry_t;
+
+typedef struct {
+    uint64_t addr;
+    uint64_t len;
+    uint32_t type;
+} __attribute__((packed)) memory_region_t;
+
+typedef void (*mmap_callback_t)(mmap_entry_t* entry, void* context);
+
+
+bool multiboot_validate(multiboot_info_t* mb_info);
+uint32_t multiboot_get_total_memory(multiboot_info_t* mb_info);
+uint32_t multiboot_get_mmap_entry_count(multiboot_info_t* mb_info);
+void multiboot_iterate_mmap(multiboot_info_t* mb_info, mmap_callback_t callback, void* context); 
+uint32_t multiboot_get_usable_regions(multiboot_info_t* mb_info, memory_region_t* regions, uint32_t max_regions);
+bool multiboot_is_memory_region_usable(memory_region_t* region, uint64_t addr, uint64_t len);
