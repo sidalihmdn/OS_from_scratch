521cf1c38ceb852aecff00ef4410e0d241416dfa
Sid-Ali Hamdane <hamdane.sidali@gmail.com>
2025-12-13 14:00:48 +0100

    added  log_error macro

--- includes/libc/log.h
+++ includes/libc/log.h
@@ -4,6 +4,7 @@
 
 
 #define LOG(msg) printk("[%s:%d] %s\n", __FILE__, __LINE__, msg);
+#define LOG_ERROR(msg) printk("[%s:%d] ERROR: %s\n", __FILE__, __LINE__, msg);
 #define LOG_F(msg, ...) printk("[%s:%d]" msg "\n", __FILE__, __LINE__, __VA_ARGS__);
 
 #endif
