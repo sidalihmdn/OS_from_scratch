a3a6e9b7ac997467be6e689ca9547390f481ae5d
Sid-Ali Hamdane <hamdane.sidali@gmail.com>
2025-12-26 23:21:27 +0100

    added some types

--- includes/unit_types.h
+++ includes/unit_types.h
@@ -6,8 +6,16 @@ typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
-typedef unsigned long uintptr_t;
 
+typedef signed char int8_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
+typedef signed long long int64_t;
+
+typedef uint32_t size_t;
+typedef int32_t ssize_t; 
+
+typedef uint32_t uintptr_t;
 
 #endif // MY_UINT_TYPES_H
 
